812a73358b057a8dc512592fc94e7a04e0f2416b
kinghuns <gaojianjun2001@hotmail.com>
2024-06-28 17:56:26 +0800

    finish bouncing balls with Evil Circle test of oojs. 2024.06.28

--- javascript/oojs/bouncing-balls/main.js
+++ javascript/oojs/bouncing-balls/main.js
@@ -1,3 +1,8 @@
+// 定义弹球计数变量
+
+const para = document.querySelector('p');
+let count = 0;
+
 // 设置画布
 
 const canvas = document.querySelector('canvas');
@@ -23,16 +28,34 @@ function randomColor() {
   return color;
 }
 
-// 定义 Ball 构造器
-
-function Ball(x, y, velX, velY, color, size) {
+//** 定义Shape构造器
+function Shape(x, y, velX, velY, exists) {
   this.x = x;
   this.y = y;
   this.velX = velX;
   this.velY = velY;
-  this.color = color;
-  this.size = size;
+  this.exists = exists;
 }
+
+// 定义 Ball 构造器
+
+// function Ball(x, y, velX, velY, color, size) {
+//   this.x = x;
+//   this.y = y;
+//   this.velX = velX;
+//   this.velY = velY;
+//   this.color = color;
+//   this.size = size;
+// }
+function Ball(x, y, velX, velY, exists, color, size) {
+  Shape.call(this, x, y, velX, velY, exists);
+
+  this.color = color;
+  this.size =size;
+}
+
+Ball.prototype = Object.create(Shape.prototype);
+Ball.prototype.constructor = Ball;
 
 // 定义彩球绘制函数
 
@@ -82,6 +105,104 @@ Ball.prototype.collisionDetect = function() {
   }
 };
 
+
+//define EvilCircle
+function EvilCircle(x, y, exists){
+  Shape.call(this, x, y, 20, 20, exists);
+
+  this.color = 'white';
+  this.size = 10;
+}
+
+EvilCircle.prototype = Object.create(Shape.prototype);
+EvilCircle.prototype.constructor = EvilCircle;
+
+
+
+// 定义 EvilCircle 绘制方法
+
+EvilCircle.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.strokeStyle = this.color;
+  ctx.lineWidth = 3;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.stroke();
+};
+
+
+// 查看EvilCircle是否超出屏幕边界
+EvilCircle.prototype.checkBounds = function() {
+  if((this.x + this.size) >= width) {
+    // this.velX = -(this.velX);
+    this.x = this.x - this.size;
+  }
+
+  if((this.x - this.size) <= 0) {
+    // this.velX = -(this.velX);
+    this.x = this.x + this.size;
+  }
+
+  if((this.y + this.size) >= height) {
+    // this.velY = -(this.velY);
+    this.y = this.y - this.size;
+  }
+
+  if((this.y - this.size) <= 0) {
+    // this.velY = -(this.velY);
+    this.y = this.y + this.size;
+  }
+
+  // this.x += this.velX;
+  // this.y += this.velY;
+};
+
+// 定义 EvilCircle 控制设置（setControls）方法
+
+EvilCircle.prototype.setControls = function() {
+  window.onkeydown = e => {
+    switch(e.key) {
+      case 'a':
+      case 'A':
+      case 'ArrowLeft':
+        this.x -= this.velX;
+        break;
+      case 'd':
+      case 'D':
+      case 'ArrowRight':
+        this.x += this.velX;
+        break;
+      case 'w':
+      case 'W':
+      case 'ArrowUp':
+        this.y -= this.velY;
+        break;
+      case 's':
+      case 'S':
+      case 'ArrowDown':
+        this.y += this.velY;
+        break;
+    }
+  };
+};
+
+// 定义碰撞检测函数
+
+EvilCircle.prototype.collisionDetect = function() {
+  for(let j = 0; j < balls.length; j++) {
+    if(balls[j].exists) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].exists = false;
+        count--;
+        para.textContent = "Remains: " + count;
+      }
+    }
+  }
+};
+
 // 定义一个数组，生成并保存所有的球
 
 let balls = [];
@@ -94,25 +215,40 @@ while(balls.length < 25) {
       random(0 + size, height - size),
       random(-7, 7),
       random(-7, 7),
+      true,
       randomColor(),
       size
   );
   balls.push(ball);
+  count++;
+  para.textContent = 'Remains: ' + count;
 }
 
 // 定义一个循环来不停地播放
 
+let evilCircle = new EvilCircle(random(0, width),random(0, height), true,)
+evilCircle.setControls();
+
+
 function loop() {
+
   ctx.fillStyle = 'rgba(0,0,0,0.25)';
   ctx.fillRect(0,0,width,height);
 
   for(let i = 0; i < balls.length; i++) {
-    balls[i].draw();
-    balls[i].update();
-    balls[i].collisionDetect();
+    if(balls[i].exists) {
+      balls[i].draw();
+      balls[i].update();
+      balls[i].collisionDetect();
+    }
   }
+
+  evilCircle.draw();
+  evilCircle.checkBounds();
+  evilCircle.collisionDetect();
 
   requestAnimationFrame(loop);
 }
 
 loop();
+
